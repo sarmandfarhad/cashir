729cddd9f5db134c411db3293d17979bc4276f8d
sarmandfarhad <sarmandfarhad@example.com>
2026-07-26 23:50:24 +0300

    fix: force https scheme in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (config('app.env') === 'production') {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
+
         if (config('database.default') === 'sqlite' && config('database.connections.sqlite.database') === ':memory:') {
             \Illuminate\Support\Facades\Artisan::call('migrate', ['--force' => true]);
             
